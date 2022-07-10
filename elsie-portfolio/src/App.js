ca84914d3c34ab55a53bffa40cd6e28c4cb6234f
ElsieMay <elsiemaylawrie1@gmail.com>
2022-07-10 16:19:58 +1000

    Added in App.js and App.scss

--- elsie-portfolio/src/App.js
+++ elsie-portfolio/src/App.js
@@ -1,10 +1,17 @@
-import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./components/Home";
+import "./App.scss";
 
 function App() {
 	return (
-		<div className="App">
-			<h1> my portofilo</h1>
-		</div>
+		<>
+			<Routes>
+				<Route path="/" element={<Layout />}>
+					<Route index element={<Home />} />
+				</Route>
+			</Routes>
+		</>
 	);
 }
 
